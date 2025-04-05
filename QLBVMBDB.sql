136fad860070312c63aa3ced4b4fe1e4d86c48f9
Thinh <heyiamthinh1003@gmail.com>
2025-04-05 10:46:44 +0700

    modify SQL in S2

--- QLBVMBDB.sql
+++ QLBVMBDB.sql
@@ -28,8 +28,9 @@ CREATE TABLE IF NOT EXISTS CHUYENBAY
 	MaChuyenBay VARCHAR(7) PRIMARY KEY,
     MaSanBayDi VARCHAR(7) NOT NULL,
     MaSanBayDen VARCHAR(7) NOT NULL,
-    NgayGioBay DATETIME NOT NULL,
-    ThoiGianBay INT, -- phut
+    NgayBay DATE NOT NULL,
+    GioBay TIME NOT NULL,
+    ThoiGianBay INT NOT NULL, -- phut
     FOREIGN KEY (MaSanBayDi) REFERENCES SANBAY(MaSanBay),
     FOREIGN KEY (MaSanBayDen) REFERENCES SANBAY(MaSanBay)
 );
@@ -50,40 +51,12 @@ CREATE TABLE IF NOT EXISTS HANGVECB
 	MaHangGhe VARCHAR(7) NOT NULL,
     MaChuyenBay VARCHAR(7) NOT NULL,
     SoLuongGhe INT NOT NULL,
+    SoLuongGheDaBan INT DEFAULT 0,
+    DonGia INT NOT NULL,
     PRIMARY KEY (MaHangGhe, MaChuyenBay),
     FOREIGN KEY (MaHangGhe) REFERENCES HANGGHE(MaHangGhe),
     FOREIGN KEY (MaChuyenBay) REFERENCES CHUYENBAY(MaChuyenBay)
 );
-
-INSERT INTO SANBAY VALUES ('SB00001', 'Sân bay California');
-INSERT INTO SANBAY VALUES ('SB00002', 'Sân bay New York');
-INSERT INTO SANBAY VALUES ('SB00003', 'Sân bay Texas');
-INSERT INTO SANBAY VALUES ('SB00004', 'Sân bay Los Angeles');
-INSERT INTO SANBAY VALUES ('SB00005', 'Sân bay Chicago O\'Hare');
-INSERT INTO SANBAY VALUES ('SB00006', 'Sân bay Atlanta Hartsfield-Jackson');
-INSERT INTO SANBAY VALUES ('SB00007', 'Sân bay Denver');
-INSERT INTO SANBAY VALUES ('SB00008', 'Sân bay Miami');
-INSERT INTO SANBAY VALUES ('SB00009', 'Sân bay Seattle-Tacoma');
-INSERT INTO SANBAY VALUES ('SB00010', 'Sân bay Washington Dulles');
-
-INSERT INTO HANGGHE VALUES ('HG00001', 'Hạng 1');
-INSERT INTO HANGGHE VALUES ('HG00002', 'Hạng 2');
-INSERT INTO HANGGHE VALUES ('HG00003', 'Hạng xoàng');
-
-INSERT INTO THAMSO VALUES (2, 30, 10, 20);
-
-SELECT * FROM SANBAY;
-SELECT * FROM HANGGHE;
-SELECT * FROM HANGVECB;
-
--- -------------------------------------------------------------------------------------------------------------------------------------------------
--- -------------------------------------------------------------------------------------------------------------------------------------------------
--- Script for Sprint 02
-alter table hangvecb 
-add column SLGheDaBan int;
-
-alter table hangvecb
-add column DonGia bigint;
 
 create table if not exists HANHKHACH
 (
@@ -100,8 +73,26 @@ create table if not exists VECHUYENBAY
     MaHanhKhach varchar(7) not null,
     MaHangGhe varchar(7) not null,
     FOREIGN KEY (MaChuyenBay) REFERENCES CHUYENBAY(MaChuyenBay),
-    FOREIGN KEY (MaHangGhe, MaChuyenBay) REFERENCES HANGVECB(MaHangGhe, MaChuyenBay)
+    FOREIGN KEY (MaHangGhe) REFERENCES HANGGHE(MaHangGhe),
+    FOREIGN KEY (MaHanhKhach) REFERENCES HANHKHACH(MaHanhKhach)
 );
+
+INSERT INTO SANBAY VALUES ('SB00001', 'Sân bay California');
+INSERT INTO SANBAY VALUES ('SB00002', 'Sân bay New York');
+INSERT INTO SANBAY VALUES ('SB00003', 'Sân bay Texas');
+INSERT INTO SANBAY VALUES ('SB00004', 'Sân bay Los Angeles');
+INSERT INTO SANBAY VALUES ('SB00005', 'Sân bay Chicago O\'Hare');
+INSERT INTO SANBAY VALUES ('SB00006', 'Sân bay Atlanta Hartsfield-Jackson');
+INSERT INTO SANBAY VALUES ('SB00007', 'Sân bay Denver');
+INSERT INTO SANBAY VALUES ('SB00008', 'Sân bay Miami');
+INSERT INTO SANBAY VALUES ('SB00009', 'Sân bay Seattle-Tacoma');
+INSERT INTO SANBAY VALUES ('SB00010', 'Sân bay Washington Dulles');
+
+INSERT INTO HANGGHE VALUES ('HG00001', 'Hạng 1');
+INSERT INTO HANGGHE VALUES ('HG00002', 'Hạng 2');
+INSERT INTO HANGGHE VALUES ('HG00003', 'Hạng xoàng');
+INSERT INTO HANGGHE VALUES ('HG00004', 'Hạng sang');
+INSERT INTO THAMSO VALUES (2, 30, 10, 20);
 
 INSERT INTO HANHKHACH (MaHanhKhach, TenHanhKhach, CMND, DienThoai) VALUES
 ('HK00001', 'Nguyen Van A', '0123456789', '0987654321'),
@@ -115,10 +106,10 @@ INSERT INTO HANHKHACH (MaHanhKhach, TenHanhKhach, CMND, DienThoai) VALUES
 ('HK00009', 'Bui Van I', '7788990011', '0988888888'),
 ('HK00010', 'Vu Thi J', '8899001122', '0999999999');
 
-
-select * from hangvecb;
-select * from sanbay;
-select * from vechuyenbay;
-select * from chuyenbay;
-
+SELECT * FROM SANBAY;
+SELECT * FROM HANGGHE;
+SELECT * FROM HANGVECB;
+SELECT * FROM CHUYENBAY;
+SELECT * FROM HANHKHACH;
+SELECT * FROM THAMSO;
 SHOW TABLES;
