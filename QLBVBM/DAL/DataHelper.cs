bac62c76b4d96e359bfc51463c01a80c022a24ba
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-04-09 15:35:30 +0700

    Fix password

--- QLBVBM/DAL/DataHelper.cs
+++ QLBVBM/DAL/DataHelper.cs
@@ -6,7 +6,7 @@ namespace QLBVBM.DAL
 {
     public class DataHelper
     {
-        private string connectionString = "server=localhost;database=QLBVMBDB;user=root;password=thinh;port=3306";
+        private string connectionString = "server=localhost;database=QLBVMBDB;user=root;password=user;port=3306";
 
         public MySqlConnection GetConnection()
         {
