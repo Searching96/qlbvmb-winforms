a7790ef1643d2ced68f00fbd1539953dccbe8854
ToanfKhoa <quachbuitoankhoa@gmail.com>
2025-04-15 15:41:12 +0700

    Add new columns to the table and new atributes to the DTO

--- QLBVBM/DTO/DTO_ThamSo.cs
+++ QLBVBM/DTO/DTO_ThamSo.cs
@@ -12,18 +12,24 @@ namespace QLBVBM.DTO
         private int? tgBayToiThieu;
         private int? tgDungToiThieu;
         private int? tgDungToiDa;
+        private int? tgDatTruocVeToiThieu;
+        private int? tgHuyDatTruocVeToiThieu;
 
-        public DTO_ThamSo(int? soSanBayTGToiDa = null, int? tgBayToiThieu = null, int? tgDungToiThieu = null, int? tgDungToiDa = null)
+        public DTO_ThamSo(int? soSanBayTGToiDa = null, int? tgBayToiThieu = null, int? tgDungToiThieu = null, int? tgDungToiDa = null, int? tgDatTruocVeToiThieu = null, int? tgHuyDatTruocVeToiThieu = null)
         {
             this.soSanBayTGToiDa = soSanBayTGToiDa;
             this.tgBayToiThieu = tgBayToiThieu;
             this.tgDungToiThieu = tgDungToiThieu;
             this.tgDungToiDa = tgDungToiDa;
+            this.tgDatTruocVeToiThieu = tgDatTruocVeToiThieu;
+            this.tgHuyDatTruocVeToiThieu = tgHuyDatTruocVeToiThieu;
         }
 
         public int? SoSanBayTGToiDa { get => soSanBayTGToiDa; set => soSanBayTGToiDa = value; }
         public int? TgBayToiThieu { get => tgBayToiThieu; set => tgBayToiThieu = value; }
         public int? TgDungToiThieu { get => tgDungToiThieu; set => tgDungToiThieu = value; }
         public int? TgDungToiDa { get => tgDungToiDa; set => tgDungToiDa = value; }
+        public int? TgDatTruocVeToiThieu { get => tgDatTruocVeToiThieu; set => tgDatTruocVeToiThieu = value; }
+        public int? TgHuyDatTruocVeToiThieu { get => tgHuyDatTruocVeToiThieu; set => tgHuyDatTruocVeToiThieu = value; }
     }
 }
