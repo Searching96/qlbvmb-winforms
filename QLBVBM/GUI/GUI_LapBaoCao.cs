bbede173591fd2aac421fe474580cbec79f5cf01
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2025-05-18 13:21:02 +0700

    UI S05

---
+++ QLBVBM/GUI/GUI_LapBaoCao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBVBM.GUI
+{
+    public partial class GUI_LapBaoCao : Form
+    {
+        public GUI_LapBaoCao()
+        {
+            InitializeComponent();
+        }
+
+        private void dtpNgayBay_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtTenHanhKhach_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2CustomGradientPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void GUI_LapBaoCao_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
