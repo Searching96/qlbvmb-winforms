7cdddf02a8dbd328a5fcc6e78f11a8e2da768ff7
Thinh <heyiamthinh1003@gmail.com>
2025-05-29 16:13:38 +0700

    Loaded UC_TiepNhan when click to btnTiepNhan

--- QLBVBM/GUI/GUI_Home.cs
+++ QLBVBM/GUI/GUI_Home.cs
@@ -12,24 +12,41 @@ namespace QLBVBM.GUI
 {
     public partial class GUI_Home: Form
     {
+        private Guna.UI2.WinForms.Guna2Panel navPanel;
+        private Guna.UI2.WinForms.Guna2Panel mainPanel;
+
         public GUI_Home()
         {
             InitializeComponent();
-            InitializeNavPanel();
+            InitializePanels();
         }
 
-        public void InitializeNavPanel()
+        private void InitializePanels()
         {
-            Guna.UI2.WinForms.Guna2Panel navPanel = new Guna.UI2.WinForms.Guna2Panel();
+            // Tạo panel nội dung chính (hiển thị UC)
+            mainPanel = new Guna.UI2.WinForms.Guna2Panel();
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.FillColor = Color.White;
+
+            // Tạo panel NavBar bên trái
+            navPanel = new Guna.UI2.WinForms.Guna2Panel();
             navPanel.Dock = DockStyle.Left;
             navPanel.Width = 200;
-            navPanel.FillColor = Color.FromArgb(45, 45, 60); // màu nền tối
+            navPanel.FillColor = Color.FromArgb(45, 45, 60);
+
+            // Thêm mainPanel trước -> chiếm phần còn lại bên phải
+            this.Controls.Add(mainPanel);
+
+            // Thêm navPanel sau -> dính trái, không bị che
             this.Controls.Add(navPanel);
 
-            // Tạo nút menu
+            InitializeNavBar();
+        }
+
+        private void InitializeNavBar()
+        {
             Guna.UI2.WinForms.Guna2Button btnTiepNhanChuyenBay = new Guna.UI2.WinForms.Guna2Button();
             btnTiepNhanChuyenBay.Text = "Tiếp nhận lịch chuyến bay";
-            //btnDashboard.Image = Properties.Resources.dashboard_icon; // hoặc null nếu chưa có
             btnTiepNhanChuyenBay.ImageOffset = new Point(10, 0);
             btnTiepNhanChuyenBay.ImageSize = new Size(24, 24);
             btnTiepNhanChuyenBay.TextOffset = new Point(15, 0);
@@ -42,6 +59,15 @@ namespace QLBVBM.GUI
             btnTiepNhanChuyenBay.TextAlign = HorizontalAlignment.Left;
             btnTiepNhanChuyenBay.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
             btnTiepNhanChuyenBay.CheckedState.FillColor = Color.FromArgb(90, 90, 120);
+
+            // Xử lý click để hiện UC
+            btnTiepNhanChuyenBay.Click += (s, e) =>
+            {
+                mainPanel.Controls.Clear();
+                var uc = new UC_TiepNhanLichChuyenBay();
+                uc.Dock = DockStyle.Fill;
+                mainPanel.Controls.Add(uc);
+            };
 
             navPanel.Controls.Add(btnTiepNhanChuyenBay);
         }
